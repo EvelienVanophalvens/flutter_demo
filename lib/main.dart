a888a9038c0c0b4e2e654f06d6123f463bb47ab5
Evelien Vanophalvens <Evelien.Vanophalvens@telenet.be>
2024-06-01 09:13:58 +0200

    Feat: check off a to do

--- lib/main.dart
+++ lib/main.dart
@@ -31,21 +31,27 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  final List<String> _items = ["test1", "test2", "test3"];
+  final List<Map<String, dynamic>> _items = [
+    {'text': 'todo1', 'checked': false},
+    {'text': 'todo2', 'checked': true},
+    {'text': 'todo3', 'checked': false},
+  ];
 
-    void _navigateToAddItemPage() async {
+  void _navigateToAddItemPage() async {
     final result = await Navigator.push(
       context,
-      MaterialPageRoute(builder: (context) => AddItemPage()),
+      MaterialPageRoute(builder: (context) => const AddItemPage()),
     );
-
+  
     if (result != null) {
       setState(() {
-        _items.add(result);
+        // Ensure that 'result' is a String before adding it to '_items'
+        if (result is String) {
+          _items.add({'text': result, 'checked': false});
+        }
       });
     }
   }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -56,29 +62,34 @@ class _MyHomePageState extends State<MyHomePage> {
       body: ListView.builder(
         itemCount: _items.length,
         itemBuilder: (context, index) {
-          return ListTile(
-            title: 
-            Container(
-              color: Theme.of(context).colorScheme.primaryContainer,
+          return CheckboxListTile(
+            title: Container(
+              color: _items[index]['checked'] ? Colors.green : Theme.of(context).colorScheme.primaryContainer,
               padding: const EdgeInsets.all(16),
-            child: Text(_items[index]),
+              child: Text(_items[index]['text']),
             ),
+            value: _items[index]['checked'],
+            onChanged: (bool? value) {
+              setState(() {
+                _items[index]['checked'] = value!;
+              });
+            },
           );
         },
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          _navigateToAddItemPage();
-        },
+        onPressed: _navigateToAddItemPage,
         tooltip: 'Add Item',
         child: const Icon(Icons.add),
       ),
     );
   }
 }
-
 class AddItemPage extends StatefulWidget {
+  const AddItemPage({super.key});
+
   @override
+  // ignore: library_private_types_in_public_api
   _AddItemPageState createState() => _AddItemPageState();
 }
 
